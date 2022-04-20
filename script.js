dcc544d236c481c63f010da251f7d1eefc4232f2
Prince Darko <padarko99@gamil.com>
2022-04-19 20:02:13 -0700

    modified portfolio

---
+++ script.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    $('#menu').click(function(){
+        $('header').toggleClass('toggle');
+    })
+
+    $(window).on('scroll load', function(){
+        $('header').removeClass('toggle');
+
+        if($(window).scrollTop() > 0){
+            $('.top').show();
+        }else{
+            $('.top').hide();
+
+        }
+    });
+
+    $('a[href*="#"]').on('click', function(e){
+        e.preventDefault();
+
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top,
+        },
+        500,
+        'linear'
+        )
+    })
+})  
